3b94791d98a6660286a7507d038738991488f040
bedirhan <bedirhan.sayy@nesbilgi.com.tr>
2024-12-09 01:20:59 +0300

    routes working on progress

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -1,55 +1,32 @@
 using EndustriApi.Entity;
+using EndustriApi.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EndustriApi.Controllers
 {
+
     [ApiController]
     [Route("api/categories")]
     public class CategoryController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetCategories()
+        private readonly ICategoryService _categoryService;
+        
+        public CategoryController(ICategoryService categoryService) 
         {
-            return Ok(new List<string> { "Category 1", "Category 2", "Category 3" });
+            _categoryService = categoryService;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _categoryService.GetAllCategories();
+            return Ok(categories);
         }
 
         [HttpPost]
         public IActionResult AddCategory(Category category)
         {
             return Ok(category);
-        }
-    }
-
-    [ApiController]
-    [Route("api/products")]
-    public class ProductController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult GetProducts()
-        {
-            return Ok(new List<string> { "Product 1", "Product 2", "Product 3" });
-        }
-    }
-
-    [ApiController]
-    [Route("api/customers")]
-    public class CustomerController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult GetCustomers()
-        {
-            return Ok(new List<string> { "Customer 1", "Customer 2", "Customer 3" });
-        }
-    }
-
-    [ApiController]
-    [Route("api/orders")]
-    public class OrderController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult GetOrders()
-        {
-            return Ok(new List<string> { "Order 1", "Order 2", "Order 3" });
         }
     }
 }
--- obj/Debug/net8.0/EndustriApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/EndustriApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EndustriApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baaa6920acdb027cd34e9b2f3ee4dcf6736f32ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("EndustriApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EndustriApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
